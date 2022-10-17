734992272827323d4a9c71bc006d3d1ee4cd3c53
Satyr <Daranmic@gmail.com>
2022-10-17 16:15:44 +0700

    third commit

---
+++ Task/Program.cs
@@ -0,0 +1,44 @@
+﻿string?[] CreateArrayString(int size)
+{
+    string?[] array = new string?[size];
+    string? text;
+    for (int i = 0; i < size; i++)
+    {
+        Console.Write("Введите элемент масива: ");
+        text = Console.ReadLine();
+        array[i] = text;
+    }
+    return array;
+}
+
+void PrintArray(string?[] array)
+{
+    Console.Write("[");
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+        else Console.Write($"{array[i]}");
+    }
+    Console.Write("]");
+}
+string?[] PrintArrayString(string?[] array)
+{
+    Console.Write("[  ");
+    for (int i = 0; i < array.Length; i++)
+    {
+        int num = 0;
+        num = array[i].Length;
+        if (num <= 3)
+        {
+            if (i < array.Length - 1) Console.Write($"{array[i]}  ");
+            if (i == array.Length - 1) Console.Write($"{array[i]}  ");
+        }
+    }
+    Console.Write("]");
+    return array;
+
+}
+string?[] arr = CreateArrayString(3);
+PrintArray(arr);
+PrintArrayString(arr);
